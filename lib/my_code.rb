b67f22a6aec492e6814e408053899efc6b54703b
jose luis <cortezluisjose@gmail.com>
2019-07-03 20:35:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # Your Code Here
+def map_to_negativize(jose_array)
+  new = []
+  i = 0
+  while i < jose_array.length do
+    new.push( jose_array[i] * -1 ) # <== Unique work
+    i += 1
+  end
+  return new
+end
